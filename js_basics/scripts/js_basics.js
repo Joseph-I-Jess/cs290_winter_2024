0f97921829e25acef5e478fe98bbb99cdce5f910
Joseph I Jess <12313215+Joseph-I-Jess@users.noreply.github.com>
2024-02-06 10:05:11 -0800

    Quick demo of some HTML 5 form elements (more on this later on the server side), then on to JavaScript basics.

---
+++ js_basics/scripts/js_basics.js
@@ -0,0 +1,11 @@
+// JavaScript functions
+function showAlert() {
+    alert('Hello, this is an alert!')
+}
+
+function changeText() {
+    var outputDiv = document.getElementById('output')
+    outputDiv.innerHTML = 'Text changed using JavaScript!'
+    alert("You just clicked that button, didn't you?!?")
+    console.log("You just clicked that button...")
+}
