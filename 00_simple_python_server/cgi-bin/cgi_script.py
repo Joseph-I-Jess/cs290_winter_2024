bd63e0322210d97f27bb67da6ae539386879a7c7
Joseph I Jess <12313215+Joseph-I-Jess@users.noreply.github.com>
2024-03-11 11:46:23 -0700

    add simple python server code

---
+++ 00_simple_python_server/cgi-bin/cgi_script.py
@@ -0,0 +1,8 @@
+#!python
+
+print("Content-type: text/html\n\n<title>Test CGI</title>\n<p style='background-color:lime;'>Hello World!</p>")
+# print()
+# print("<title>Test CGI</title>")
+# print('<p style="background-color:lime;">Hello World!</p>')
+
+#print("\nHello World!")  #minimal to print contents... needs that newline for some reason
---
+++ js_basics/more_js.js
@@ -0,0 +1,10 @@
+function myDisplayer(some) {
+    document.getElementById("demo").innerHTML = some;
+  }
+  
+  function myCalculator(num1, num2, myCallback) {
+    let sum = num1 + num2;
+    myCallback(sum);
+  }
+  
+  myCalculator(5, 5, myDisplayer);
